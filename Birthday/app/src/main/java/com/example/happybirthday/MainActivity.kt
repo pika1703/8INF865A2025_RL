100afc84180a9309d514d53009bbac790a52bb33
Romain LE SOURD <romain.lesourd@gmail.com>
2025-09-22 22:59:59 -0400

    Etape 9 : marges et alignement du texte + affichage sur le téléphone

--- Birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -22,24 +28,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                 {
+                     Surface(
+                         modifier = Modifier.fillMaxSize(),
+                         color = MaterialTheme.colorScheme.background
+                     ) {
+                         GreetingText(message = "Happy Birthday mon Ami!",
+                             from = "From Romain",
+                             modifier = Modifier.padding(8.dp))
+                     }
                 }
             }
         }
     }
-}
 
 @Composable
 fun GreetingText(modifier: Modifier = Modifier, message: String, from: String){
-    Column(modifier = modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Text(
             text = message,
-            fontSize = 100.sp,
+            fontSize = 80.sp,
             lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = modifier.padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
