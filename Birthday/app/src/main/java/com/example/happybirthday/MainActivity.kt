1f5965ed420e214c9472324f55befff6ea790ab0
Romain LE SOURD <romain.lesourd@gmail.com>
2025-09-23 14:08:22 -0400

    Etape 2.5 : echelle et opacité de l image

--- Birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -71,7 +72,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
     Box(modifier) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         GreetingText(message = message,
             from = from,
