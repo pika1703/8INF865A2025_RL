d294c0984046714028c16ca15ca60b269309d180
Romain LE SOURD <romain.lesourd@gmail.com>
2025-09-23 15:11:35 -0400

    Etape 2.7 : transformation des string en ressources du projet

--- Birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,8 +37,8 @@ class MainActivity : ComponentActivity() {
                          modifier = Modifier.fillMaxSize(),
                          color = MaterialTheme.colorScheme.background
                      ) {
-                         GreetingImage(message = "Happy Birthday mon Ami!",
-                             from = "From Romain",
+                         GreetingImage(message = stringResource(R.string.happy_birthday_text),
+                             from = stringResource(R.string.signature_text),
                              modifier = Modifier.padding(8.dp))
                      }
                 }
@@ -80,7 +81,7 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
             from = from,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(8.dp))
+                .padding(16.dp))
     }
 }
 
@@ -88,6 +89,8 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday mon Ami!", from = "From Romain")
+        GreetingImage( message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
+        )
     }
 }
