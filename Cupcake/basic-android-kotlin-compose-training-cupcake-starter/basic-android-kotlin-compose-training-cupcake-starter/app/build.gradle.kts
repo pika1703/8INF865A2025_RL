648be3e636bdccec31f045b5a014731cf95f8643
Romain LE SOURD <romain.lesourd@gmail.com>
2025-10-20 17:51:23 -0400

    Etape 4 : configuration de NavHost

--- Cupcake/basic-android-kotlin-compose-training-cupcake-starter/basic-android-kotlin-compose-training-cupcake-starter/app/build.gradle.kts
+++ Cupcake/basic-android-kotlin-compose-training-cupcake-starter/basic-android-kotlin-compose-training-cupcake-starter/app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.navigation:navigation-compose-android:2.9.5")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
