c41f724c02b491363b4b31c8b379b999d6e9569d
Romain LE SOURD <romain.lesourd@gmail.com>
2025-10-20 20:18:08 -0400

    Etape 7 : verif du mot à deviner et couleur et texte si c est faux

--- Unscramble/basic-android-kotlin-compose-training-unscramble-starter/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ Unscramble/basic-android-kotlin-compose-training-unscramble-starter/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -81,9 +81,10 @@ fun GameScreen(
         )
         GameLayout(
             onUserGuessChanged = { gameViewModel.updateUserGuess(it) },
-            onKeyboardDone = { },
+            onKeyboardDone = { gameViewModel.checkUserGuess() },
             currentScrambledWord = gameUiState.currentScrambledWord,
             userGuess = gameViewModel.userGuess,
+            isGuessWrong = gameUiState.isGuessedWordWrong,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
@@ -99,7 +100,7 @@ fun GameScreen(
 
             Button(
                 modifier = Modifier.fillMaxWidth(),
-                onClick = { }
+                onClick = { gameViewModel.checkUserGuess() }
             ) {
                 Text(
                     text = stringResource(R.string.submit),
@@ -139,6 +140,7 @@ fun GameStatus(score: Int, modifier: Modifier = Modifier) {
 fun GameLayout(onUserGuessChanged: (String) -> Unit,
                onKeyboardDone: () -> Unit,
                currentScrambledWord: String,
+               isGuessWrong: Boolean,
                userGuess: String,
                modifier: Modifier = Modifier) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
@@ -182,8 +184,14 @@ fun GameLayout(onUserGuessChanged: (String) -> Unit,
                     disabledContainerColor = colorScheme.surface,
                 ),
                 onValueChange = onUserGuessChanged,
-                label = { Text(stringResource(R.string.enter_your_word)) },
-                isError = false,
+                label = {
+                    if (isGuessWrong) {
+                        Text(stringResource(R.string.wrong_guess))
+                    } else {
+                        Text(stringResource(R.string.enter_your_word))
+                    }
+                },
+                isError = isGuessWrong,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
